303b0c94a756fa421dbbf744a5112b3f4b0ca2dd
Adhul Sanjay <adhulcsanjay1728@gmail.com>
2026-02-03 22:55:49 +0530

    commit

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -18,8 +18,8 @@ export async function POST(request: Request) {
     const res = NextResponse.json({ success: true });
     res.cookies.set("admin_token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      secure: true,        // ALWAYS true on Vercel
+  sameSite: "none", 
       maxAge: 60 * 60 * 24 * 7,
       path: "/",
     });
